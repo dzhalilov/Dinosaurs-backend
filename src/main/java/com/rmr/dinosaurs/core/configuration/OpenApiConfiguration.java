9fe7eda36a3663b90a83b26c1c70df032def756f
apkuznetsov <ethenaut@ya.ru>
2022-10-08 23:33:38 +0400

    set up swagger

---
+++ src/main/java/com/rmr/dinosaurs/core/configuration/OpenApiConfiguration.java
@@ -0,0 +1,18 @@
+package com.rmr.dinosaurs.core.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+  @Bean
+  public OpenAPI customOpenApi() {
+    return new OpenAPI()
+            .info(new Info()
+                    .title("Example Swagger API")
+                    .version("0.0.1")
+            );
+  }
+}
--- src/main/java/com/rmr/dinosaurs/core/configuration/SecurityConfiguration.java
+++ src/main/java/com/rmr/dinosaurs/core/configuration/SecurityConfiguration.java
@@ -1,10 +1,27 @@
 package com.rmr.dinosaurs.core.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
+  private static final String[] AUTH_WHITELIST = {
+    "/swagger-ui.html",
+    "/swagger-ui/**",
+    "/swagger-resources/**",
+    "/v2/api-docs/**",
+    "/v3/api-docs/**",
+  };
 
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+      .antMatchers(AUTH_WHITELIST).permitAll()
+      .antMatchers("/**").authenticated();
+    return http.build();
+  }
 }
