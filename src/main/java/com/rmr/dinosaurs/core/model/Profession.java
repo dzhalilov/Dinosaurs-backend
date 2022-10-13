7402a18328dafc3777550351c124a862c34935b1
Andrey <ethenaut@ya.ru>
2022-10-13 11:56:22 +0400

    def profession entity

---
+++ src/main/java/com/rmr/dinosaurs/core/model/Profession.java
@@ -0,0 +1,37 @@
+package com.rmr.dinosaurs.core.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "profession")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Profession {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "id", nullable = false, unique = true)
+  int id;
+
+  @Column(name = "name", nullable = false, unique = true)
+  String name;
+
+  @Column(name = "cover_url", nullable = false)
+  String coverUrl;
+
+  @Column(name = "short_description", nullable = false)
+  String shortDescription;
+
+  @Column(name = "description", nullable = false)
+  String description;
+
+}
