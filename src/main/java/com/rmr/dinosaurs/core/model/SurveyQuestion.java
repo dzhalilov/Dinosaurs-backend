1341078dd056cf0d31109c69f8ac6357355c893e
Andrey <ethenaut@ya.ru>
2022-10-13 11:55:37 +0400

    def survey question entity

--- src/main/java/com/rmr/dinosaurs/core/model/SurveyQuestion.java
+++ src/main/java/com/rmr/dinosaurs/core/model/SurveyQuestion.java
@@ -1,6 +1,5 @@
 package com.rmr.dinosaurs.core.model;
 
-import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -8,39 +7,29 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Entity
-@Table(name = "survey")
+@Table(name = "survey_question")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Survey {
+public class SurveyQuestion {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   @Column(name = "id", nullable = false, unique = true)
   int id;
 
-  @Column(name = "title", nullable = false)
-  String title;
+  @Column(name = "text", nullable = false)
+  String text;
 
-  @Column(name = "short_description", nullable = false)
-  String shortDescription;
-
-  @Column(name = "description", nullable = false)
-  String description;
-
-  @OneToMany(fetch = FetchType.LAZY)
-  @JoinColumn(name = "sq_survey_id")
-  @EqualsAndHashCode.Exclude
-  @ToString.Exclude
-  Set<SurveyQuestion> questions;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "sq_survey_id", updatable = false)
+  Survey survey;
 
 }
