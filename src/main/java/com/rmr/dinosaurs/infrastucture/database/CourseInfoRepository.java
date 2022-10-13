a6a5357d3fa85b131ad0ec084b0a605d18bd5552
Andrey <ethenaut@ya.ru>
2022-10-13 15:07:31 +0400

    def course info repo

---
+++ src/main/java/com/rmr/dinosaurs/infrastucture/database/CourseInfoRepository.java
@@ -0,0 +1,10 @@
+package com.rmr.dinosaurs.infrastucture.database;
+
+import com.rmr.dinosaurs.core.model.CourseInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseInfoRepository extends JpaRepository<CourseInfo, Integer> {
+
+}
