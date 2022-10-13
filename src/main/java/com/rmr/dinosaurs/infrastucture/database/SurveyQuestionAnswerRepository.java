90aa12577dbb7cf0927f3e1b4d18be84587201a4
Andrey <ethenaut@ya.ru>
2022-10-13 15:06:51 +0400

    def survey question answer repo

---
+++ src/main/java/com/rmr/dinosaurs/infrastucture/database/SurveyQuestionAnswerRepository.java
@@ -0,0 +1,11 @@
+package com.rmr.dinosaurs.infrastucture.database;
+
+import com.rmr.dinosaurs.core.model.SurveyQuestionAnswer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SurveyQuestionAnswerRepository
+    extends JpaRepository<SurveyQuestionAnswer, Integer> {
+
+}
