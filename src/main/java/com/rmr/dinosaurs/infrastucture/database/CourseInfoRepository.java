965fbf00e9ac93790b083fc25c81b02c28aafa59
ffibonacci <fadedfacet@gmail.com>
2022-10-14 12:59:57 +0300

    repositories refactoring following entities changes

--- src/main/java/com/rmr/dinosaurs/infrastucture/database/CourseInfoRepository.java
+++ src/main/java/com/rmr/dinosaurs/infrastucture/database/CourseInfoRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CourseInfoRepository extends JpaRepository<CourseInfo, Integer> {
+public interface CourseInfoRepository extends JpaRepository<CourseInfo, Long> {
 
 }
--- src/main/java/com/rmr/dinosaurs/infrastucture/database/UserRepository.java
+++ src/main/java/com/rmr/dinosaurs/infrastucture/database/UserRepository.java
@@ -1,11 +1,13 @@
 package com.rmr.dinosaurs.infrastucture.database;
 
 import com.rmr.dinosaurs.core.model.User;
-import java.util.UUID;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, Long> {
+
+  Optional<User> findByEmailIgnoreCase(String email);
 
 }
