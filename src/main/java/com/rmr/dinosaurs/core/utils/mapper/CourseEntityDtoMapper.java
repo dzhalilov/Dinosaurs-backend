1b95177fc921748caf9ff96b6f9820f8a6d96d14
Andrey <ethenaut@ya.ru>
2022-10-16 21:57:29 +0400

    ignore unmapped fields in course mapper

--- src/main/java/com/rmr/dinosaurs/core/utils/mapper/CourseEntityDtoMapper.java
+++ src/main/java/com/rmr/dinosaurs/core/utils/mapper/CourseEntityDtoMapper.java
@@ -4,12 +4,23 @@ import com.rmr.dinosaurs.core.model.Course;
 import com.rmr.dinosaurs.core.model.dto.CourseDto;
 import com.rmr.dinosaurs.core.model.dto.CreatingCourseDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CourseEntityDtoMapper {
 
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "shortDescription", ignore = true)
+  @Mapping(target = "internalRating", ignore = true)
+  @Mapping(target = "isIndefinite", ignore = true)
+  @Mapping(target = "isArchived", ignore = true)
+  @Mapping(target = "provider", ignore = true)
+  @Mapping(target = "courseAndProfessionRefs", ignore = true)
+  @Mapping(target = "courseAndTagRefs", ignore = true)
   Course toEntity(CreatingCourseDto dto);
 
+  @Mapping(target = "providerUrl", ignore = true)
+  @Mapping(target = "professionName", ignore = true)
   CourseDto toDto(Course entity);
 
 }
