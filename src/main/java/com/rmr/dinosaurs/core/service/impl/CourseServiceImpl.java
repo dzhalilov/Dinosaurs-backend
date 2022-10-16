b6f575241658e8050254f2f09f8a94207f3da140
Andrey <ethenaut@ya.ru>
2022-10-16 13:16:21 +0400

    impl course service's "save new tags and save new cat refs"

--- src/main/java/com/rmr/dinosaurs/core/service/impl/CourseServiceImpl.java
+++ src/main/java/com/rmr/dinosaurs/core/service/impl/CourseServiceImpl.java
@@ -2,14 +2,20 @@ package com.rmr.dinosaurs.core.service.impl;
 
 import com.rmr.dinosaurs.core.model.Course;
 import com.rmr.dinosaurs.core.model.CourseAndProfession;
+import com.rmr.dinosaurs.core.model.CourseAndTag;
 import com.rmr.dinosaurs.core.model.CourseProvider;
 import com.rmr.dinosaurs.core.model.Profession;
+import com.rmr.dinosaurs.core.model.Tag;
 import com.rmr.dinosaurs.core.model.dto.CourseDto;
 import com.rmr.dinosaurs.core.model.dto.CreatingCourseDto;
 import com.rmr.dinosaurs.core.service.CourseService;
 import com.rmr.dinosaurs.infrastucture.database.CourseAndProfessionRepository;
+import com.rmr.dinosaurs.infrastucture.database.CourseAndTagRepository;
 import com.rmr.dinosaurs.infrastucture.database.CourseProviderRepository;
 import com.rmr.dinosaurs.infrastucture.database.ProfessionRepository;
+import com.rmr.dinosaurs.infrastucture.database.TagRepository;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,7 +28,9 @@ public class CourseServiceImpl implements CourseService {
 
   private final CourseProviderRepository providerRepo;
   private final ProfessionRepository professionRepo;
+  private final TagRepository tagRepo;
   private final CourseAndProfessionRepository capRefRepo;
+  private final CourseAndTagRepository catRefRepo;
 
   @Override
   public CourseDto addCourse(CreatingCourseDto course) {
@@ -71,6 +79,44 @@ public class CourseServiceImpl implements CourseService {
       capRef.setProfession(profession);
       capRefRepo.save(capRef);
     }
+  }
+
+  private void saveNewTagsAndSaveNewCatRefs(Course course, String[] tagValues) {
+    boolean isEmptyTags = tagValues == null || tagValues.length == 0;
+    if (isEmptyTags) {
+      return;
+    }
+
+    final int len = tagValues.length;
+    List<Tag> savingTags = new ArrayList<>(len);
+    List<Tag> existingTags = new ArrayList<>(len);
+
+    for (String v : tagValues) {
+      Optional<Tag> optTag = tagRepo.findByValue(v);
+
+      if (optTag.isEmpty()) {
+        Tag newTag = new Tag();
+        newTag.setValue(v);
+        savingTags.add(newTag);
+      } else {
+        existingTags.add(
+            optTag.get()
+        );
+      }
+    }
+
+    List<Tag> savedTags = tagRepo.saveAllAndFlush(savingTags);
+    existingTags.addAll(savedTags);
+
+    List<CourseAndTag> catRefs = new ArrayList<>(len);
+    for (Tag t : existingTags) {
+      CourseAndTag catRef = new CourseAndTag();
+      catRef.setCourse(course);
+      catRef.setTag(t);
+      catRefs.add(catRef);
+    }
+
+    catRefRepo.saveAll(catRefs);
   }
 
 }
