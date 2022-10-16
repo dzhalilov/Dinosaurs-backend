fd9216a04a07890bec7b5adfc1af5597366db94d
Andrey <ethenaut@ya.ru>
2022-10-16 13:06:56 +0400

    impl course service's "find course provider or save new and flush"

--- src/main/java/com/rmr/dinosaurs/core/service/impl/CourseServiceImpl.java
+++ src/main/java/com/rmr/dinosaurs/core/service/impl/CourseServiceImpl.java
@@ -1,8 +1,11 @@
 package com.rmr.dinosaurs.core.service.impl;
 
+import com.rmr.dinosaurs.core.model.CourseProvider;
 import com.rmr.dinosaurs.core.model.dto.CourseDto;
 import com.rmr.dinosaurs.core.model.dto.CreatingCourseDto;
 import com.rmr.dinosaurs.core.service.CourseService;
+import com.rmr.dinosaurs.infrastucture.database.CourseProviderRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,9 +15,27 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CourseServiceImpl implements CourseService {
 
+  private final CourseProviderRepository providerRepo;
+
   @Override
   public CourseDto addCourse(CreatingCourseDto course) {
     return null;
   }
-  
+
+  private CourseProvider findCourseProviderOrSaveNewAndFlush(String providerUrl) {
+    Optional<CourseProvider> optFoundProvider = providerRepo
+        .findByUrl(providerUrl);
+
+    CourseProvider provider;
+    if (optFoundProvider.isEmpty()) {
+      provider = new CourseProvider();
+      provider.setUrl(providerUrl);
+      provider = providerRepo.saveAndFlush(provider);
+    } else {
+      provider = optFoundProvider.get();
+    }
+
+    return provider;
+  }
+
 }
--- src/main/java/com/rmr/dinosaurs/infrastucture/database/CourseProviderRepository.java
+++ src/main/java/com/rmr/dinosaurs/infrastucture/database/CourseProviderRepository.java
@@ -1,10 +1,13 @@
 package com.rmr.dinosaurs.infrastucture.database;
 
 import com.rmr.dinosaurs.core.model.CourseProvider;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CourseProviderRepository extends JpaRepository<CourseProvider, Long> {
+
+  Optional<CourseProvider> findByUrl(String url);
 
 }
