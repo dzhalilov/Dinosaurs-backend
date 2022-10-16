2a1f2dc2d4aa90d9ebc0cc77a0753f321694f8a5
Andrey <ethenaut@ya.ru>
2022-10-16 13:10:12 +0400

    impl course service's "find profession or save new and flush"

--- src/main/java/com/rmr/dinosaurs/core/service/impl/CourseServiceImpl.java
+++ src/main/java/com/rmr/dinosaurs/core/service/impl/CourseServiceImpl.java
@@ -1,10 +1,12 @@
 package com.rmr.dinosaurs.core.service.impl;
 
 import com.rmr.dinosaurs.core.model.CourseProvider;
+import com.rmr.dinosaurs.core.model.Profession;
 import com.rmr.dinosaurs.core.model.dto.CourseDto;
 import com.rmr.dinosaurs.core.model.dto.CreatingCourseDto;
 import com.rmr.dinosaurs.core.service.CourseService;
 import com.rmr.dinosaurs.infrastucture.database.CourseProviderRepository;
+import com.rmr.dinosaurs.infrastucture.database.ProfessionRepository;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +18,7 @@ import org.springframework.stereotype.Service;
 public class CourseServiceImpl implements CourseService {
 
   private final CourseProviderRepository providerRepo;
+  private final ProfessionRepository professionRepo;
 
   @Override
   public CourseDto addCourse(CreatingCourseDto course) {
@@ -36,6 +39,22 @@ public class CourseServiceImpl implements CourseService {
     }
 
     return provider;
+  }
+
+  private Profession findProfessionOrSaveNewAndFlush(String professionName) {
+    Optional<Profession> optFoundProfession = professionRepo
+        .findByName(professionName);
+
+    Profession profession;
+    if (optFoundProfession.isEmpty()) {
+      profession = new Profession();
+      profession.setName(professionName);
+      profession = professionRepo.saveAndFlush(profession);
+    } else {
+      profession = optFoundProfession.get();
+    }
+
+    return profession;
   }
 
 }
--- src/main/java/com/rmr/dinosaurs/infrastucture/database/ProfessionRepository.java
+++ src/main/java/com/rmr/dinosaurs/infrastucture/database/ProfessionRepository.java
@@ -1,10 +1,13 @@
 package com.rmr.dinosaurs.infrastucture.database;
 
 import com.rmr.dinosaurs.core.model.Profession;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProfessionRepository extends JpaRepository<Profession, Long> {
+
+  Optional<Profession> findByName(String professionName);
 
 }
