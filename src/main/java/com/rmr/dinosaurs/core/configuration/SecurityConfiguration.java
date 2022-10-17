a86ea17227c1556a805ee1bdcae3ae3b4e20ba82
ffibonacci <fadedfacet@gmail.com>
2022-10-17 15:43:57 +0300

    configure cors source

--- src/main/java/com/rmr/dinosaurs/core/configuration/SecurityConfiguration.java
+++ src/main/java/com/rmr/dinosaurs/core/configuration/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.rmr.dinosaurs.core.configuration;
 import com.rmr.dinosaurs.core.auth.security.JwtTokenFilter;
 import com.rmr.dinosaurs.core.auth.security.JwtTokenProvider;
 import com.rmr.dinosaurs.core.auth.security.JwtTokenService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -14,6 +15,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder.BCryptVe
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -52,6 +56,17 @@ public class SecurityConfiguration {
   @Bean
   public PasswordEncoder getPasswordEncoder() {
     return new BCryptPasswordEncoder(BCryptVersion.$2Y, 12);
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.addAllowedOriginPattern(CorsConfiguration.ALL);
+    configuration.setAllowedMethods(List.of(CorsConfiguration.ALL));
+    configuration.setAllowedHeaders(List.of(CorsConfiguration.ALL));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
 }
