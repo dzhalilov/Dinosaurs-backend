8d4002838b878789b07dc7502f906572d90eed4c
Andrey <ethenaut@ya.ru>
2022-10-18 01:13:52 +0400

    def and impl course provider service's "create provider"

---
+++ src/main/java/com/rmr/dinosaurs/core/service/CourseProviderService.java
@@ -0,0 +1,9 @@
+package com.rmr.dinosaurs.core.service;
+
+import com.rmr.dinosaurs.core.model.dto.CourseProviderDto;
+
+public interface CourseProviderService {
+
+  CourseProviderDto createProvider(CourseProviderDto provider);
+
+}
---
+++ src/main/java/com/rmr/dinosaurs/core/service/impl/CourseProviderServiceImpl.java
@@ -0,0 +1,28 @@
+package com.rmr.dinosaurs.core.service.impl;
+
+import com.rmr.dinosaurs.core.model.CourseProvider;
+import com.rmr.dinosaurs.core.model.dto.CourseProviderDto;
+import com.rmr.dinosaurs.core.service.CourseProviderService;
+import com.rmr.dinosaurs.core.utils.mapper.CourseProviderEntityDtoMapper;
+import com.rmr.dinosaurs.infrastucture.database.CourseProviderRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class CourseProviderServiceImpl implements CourseProviderService {
+
+  private final CourseProviderEntityDtoMapper mapper;
+
+  private final CourseProviderRepository providerRepo;
+
+  @Override
+  public CourseProviderDto createProvider(CourseProviderDto dto) {
+    CourseProvider newProvider = mapper.toEntity(dto);
+    CourseProvider savedProvider = providerRepo.saveAndFlush(newProvider);
+    return mapper.toDto(savedProvider);
+  }
+
+}
