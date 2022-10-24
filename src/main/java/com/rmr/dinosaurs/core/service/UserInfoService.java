d87c6fabe2536e28a66174f77fd834ff286be8ca
ffibonacci <fadedfacet@gmail.com>
2022-10-24 13:49:11 +0300

    add edit user profile method and methods descriptions

--- src/main/java/com/rmr/dinosaurs/core/service/UserInfoService.java
+++ src/main/java/com/rmr/dinosaurs/core/service/UserInfoService.java
@@ -6,11 +6,41 @@ import java.util.List;
 
 public interface UserInfoService {
 
+  /**
+   * get current user full profile info
+   *
+   * @return user profile data
+   */
   UserInfoDto getMyProfile();
 
+  /**
+   * set current user profile data
+   *
+   * @param userInfoDto to be changed to
+   * @return changed user profile data
+   */
+  UserInfoDto editMyProfile(UserInfoDto userInfoDto);
+
+  /**
+   * get users profile data by user info id
+   *
+   * @param id user info id
+   * @return users profile data
+   */
   UserInfoDto getUserInfoById(Long id);
 
+  /**
+   * get list of all users profiles
+   *
+   * @return users profile data in a short representation
+   */
   List<ShortUserInfoDto> getAllUserInfos();
 
+  /**
+   * get list of all moderators profiles
+   *
+   * @return moderators profile data in a short representation
+   */
   List<ShortUserInfoDto> getAllModerators();
+
 }
