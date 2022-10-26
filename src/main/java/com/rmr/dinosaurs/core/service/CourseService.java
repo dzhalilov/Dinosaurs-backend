39d73a97c963801fe9834d34c712ed20db798e0f
Andrey <ethenaut@ya.ru>
2022-10-26 11:50:21 +0400

    doc course service

--- src/main/java/com/rmr/dinosaurs/core/service/CourseService.java
+++ src/main/java/com/rmr/dinosaurs/core/service/CourseService.java
@@ -8,14 +8,46 @@ import java.util.List;
 
 public interface CourseService {
 
-  CourseCreateUpdateDto addCourse(CourseCreateUpdateDto course);
+  /**
+   * create course profile data
+   *
+   * @param courseDto to be created
+   * @return created course profile data
+   */
+  CourseCreateUpdateDto addCourse(CourseCreateUpdateDto courseDto);
 
-  CourseReadDto getCourseById(long id);
+  /**
+   * get course profile data by course id
+   *
+   * @param courseId course profiler id
+   * @return course profile
+   */
+  CourseReadDto getCourseById(long courseId);
 
-  CourseCreateUpdateDto editCourseById(long id, CourseCreateUpdateDto dto);
+  /**
+   * set course profile data
+   *
+   * @param courseId  course profile id to be changed to
+   * @param courseDto to be changed to
+   * @return changed course profile data
+   */
+  CourseCreateUpdateDto editCourseById(long courseId, CourseCreateUpdateDto courseDto);
 
+  /**
+   * get list of all course profiles
+   *
+   * @return list of course profile data
+   */
   List<CourseReadDto> getAllCourses();
 
+  /**
+   * get page of filtered course profiles
+   *
+   * @param pageNum page number
+   * @param sortBy  sort by startsAt or endsAt param
+   * @param filter  filter params
+   * @return page of filtered course profile data
+   */
   CourseReadPageDto getFilteredCoursePage(int pageNum, String sortBy, FilterParamsDto filter);
 
 }
