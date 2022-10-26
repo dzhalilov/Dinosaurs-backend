003c31f63b37f9ce2cedff5e4d288d999451bb21
Andrey <ethenaut@ya.ru>
2022-10-26 11:48:05 +0400

    doc course provider service

--- src/main/java/com/rmr/dinosaurs/core/service/CourseProviderService.java
+++ src/main/java/com/rmr/dinosaurs/core/service/CourseProviderService.java
@@ -6,14 +6,44 @@ import java.util.List;
 
 public interface CourseProviderService {
 
-  ProviderDto addProvider(ProviderDto provider);
+  /**
+   * create course provider profile data
+   *
+   * @param providerDto to be created
+   * @return created course provider profile data
+   */
+  ProviderDto addProvider(ProviderDto providerDto);
 
-  ProviderDto getProviderById(long id);
+  /**
+   * get course provider profile data by provider id
+   *
+   * @param providerId course provider profile id
+   * @return course provider profile
+   */
+  ProviderDto getProviderById(long providerId);
 
-  ProviderDto editProviderById(long id, ProviderDto dto);
+  /**
+   * set course provider profile data
+   *
+   * @param providerId  course provider profile id to be changed to
+   * @param providerDto data to be changed to
+   * @return changed course provider profile data
+   */
+  ProviderDto editProviderById(long providerId, ProviderDto providerDto);
 
+  /**
+   * get list of all course provider profiles
+   *
+   * @return list of course provider profile data
+   */
   List<ProviderDto> getAllProviders();
 
+  /**
+   * get page of course provider profiles
+   *
+   * @param pageNum page number
+   * @return page of course provider profile data
+   */
   ProviderPageDto getProviderPage(int pageNum);
 
 }
