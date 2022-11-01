4587853eca82a7a31bcaaf101067fd35d85f3228
ffibonacci <fadedfacet@gmail.com>
2022-11-01 16:45:33 +0300

    add javadoc

--- src/main/java/com/rmr/dinosaurs/domain/auth/service/UserService.java
+++ src/main/java/com/rmr/dinosaurs/domain/auth/service/UserService.java
@@ -5,12 +5,35 @@ import java.util.List;
 
 public interface UserService {
 
+  /**
+   * get current user
+   *
+   * @return UserDto
+   */
   UserDto getCurrentUserDto();
 
+  /**
+   * get user by id
+   *
+   * @param id user id
+   * @return UserDto
+   */
   UserDto getUserById(Long id);
 
+  /**
+   * return all confirmed users
+   *
+   * @return list of UserDto
+   */
   List<UserDto> getAllUsers();
 
+  /**
+   * set or unset user as moderator
+   *
+   * @param id          user id
+   * @param isModerator flag to be set
+   * @return updated UserDto
+   */
   UserDto setUserModerator(Long id, Boolean isModerator);
 
 }
