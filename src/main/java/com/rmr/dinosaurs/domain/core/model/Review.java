e19ccc9b4b1283a88266e494401d105398e51150
Ilyas Dzhalilov <dzhalilov.ilyas@gmail.com>
2022-11-05 17:00:01 +0300

    feat: add rating to Course, add Review

---
+++ src/main/java/com/rmr/dinosaurs/domain/core/model/Review.java
@@ -0,0 +1,42 @@
+package com.rmr.dinosaurs.domain.core.model;
+
+import com.rmr.dinosaurs.domain.userinfo.model.UserInfo;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "review")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Review {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "id", nullable = false, unique = true)
+  Long id;
+
+  @Column(name = "rating", nullable = false)
+  @Size(min = 1, max = 5)
+  Integer rating;
+
+  @Column(name = "text_review")
+  @Length(max = 1000)
+  String textReview;
+
+  @ManyToOne
+  @JoinColumn(name = "course_id")
+  Course course;
+
+  @ManyToOne
+  @JoinColumn(name = "user_info_id")
+  UserInfo userInfo;
+
+}
