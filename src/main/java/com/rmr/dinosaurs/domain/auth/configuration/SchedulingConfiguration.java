e896d8959302c6226368765dd739d2782597ac3e
ffibonacci <fadedfacet@gmail.com>
2022-11-08 12:46:51 +0300

    add scheduling config

---
+++ src/main/java/com/rmr/dinosaurs/domain/auth/configuration/SchedulingConfiguration.java
@@ -0,0 +1,27 @@
+package com.rmr.dinosaurs.domain.auth.configuration;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfiguration implements SchedulingConfigurer {
+
+  private static final int THREAD_AMOUNT = 2;
+
+  @Bean(destroyMethod = "shutdown")
+  public ExecutorService taskExecutor() {
+    return Executors.newScheduledThreadPool(THREAD_AMOUNT);
+  }
+
+  @Override
+  public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+    taskRegistrar.setScheduler(taskExecutor());
+  }
+
+}
