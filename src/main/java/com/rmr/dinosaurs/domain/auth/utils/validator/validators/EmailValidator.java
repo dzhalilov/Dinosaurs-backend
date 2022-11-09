8af62a52724c8506805806775507208621b19251
ffibonacci <fadedfacet@gmail.com>
2022-11-09 12:29:40 +0300

    change validation regex

--- src/main/java/com/rmr/dinosaurs/domain/auth/utils/validator/validators/EmailValidator.java
+++ src/main/java/com/rmr/dinosaurs/domain/auth/utils/validator/validators/EmailValidator.java
@@ -9,10 +9,12 @@ public class EmailValidator implements
     ConstraintValidator<EmailConstraintValidator, String> {
 
   /**
-   * RFC 5322 standard following regex pattern
+   * RFC 5322 standard following regex pattern regex from
+   * https://www.baeldung.com/java-email-validation-regex#strict-regular-expression-validation
    */
   protected static final Pattern emailPattern =
-      Pattern.compile("^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$");
+      Pattern.compile("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+          + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$");
 
   @Override
   public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
