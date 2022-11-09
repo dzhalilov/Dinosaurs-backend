fd8522ffb7399fbf514e6dc273ac421913bc44a1
ffibonacci <fadedfacet@gmail.com>
2022-11-09 18:17:13 +0300

    change to path variable and response status add

--- src/main/java/com/rmr/dinosaurs/presentation/web/statistics/StatisticsController.java
+++ src/main/java/com/rmr/dinosaurs/presentation/web/statistics/StatisticsController.java
@@ -2,9 +2,11 @@ package com.rmr.dinosaurs.presentation.web.statistics;
 
 import com.rmr.dinosaurs.domain.statistics.service.CourseStatisticsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,7 +17,8 @@ public class StatisticsController {
   private final CourseStatisticsService courseStatisticsService;
 
   @PostMapping("/course/{courseId}")
-  public void registerCourseTransition(@RequestParam Long courseId) {
+  @ResponseStatus(HttpStatus.CREATED)
+  public void registerCourseTransition(@PathVariable Long courseId) {
     courseStatisticsService.courseTransitionRegister(courseId);
   }
 
