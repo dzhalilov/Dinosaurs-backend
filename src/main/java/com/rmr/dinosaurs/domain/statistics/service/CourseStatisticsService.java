2deae4a07cf09d8f790d7f0de4294850f9c9b58e
ffibonacci <fadedfacet@gmail.com>
2022-11-13 21:37:21 +0300

    add implementation

--- src/main/java/com/rmr/dinosaurs/domain/statistics/service/CourseStatisticsService.java
+++ src/main/java/com/rmr/dinosaurs/domain/statistics/service/CourseStatisticsService.java
@@ -1,7 +1,24 @@
 package com.rmr.dinosaurs.domain.statistics.service;
 
+import com.rmr.dinosaurs.domain.statistics.model.dto.CourseLinkTransitionFilterDto;
+import com.rmr.dinosaurs.domain.statistics.model.dto.CourseLinkTransitionPageDto;
+
 public interface CourseStatisticsService {
 
+  /**
+   * register course link transition
+   *
+   * @param courseId course id
+   */
   void courseTransitionRegister(Long courseId);
+
+  /**
+   * get all course link transitions by filter
+   *
+   * @param courseLinkTransitionFilterDto search criteria
+   * @return course link transition page
+   */
+  CourseLinkTransitionPageDto getAllCourseLinkTransitionsByFilter(
+      CourseLinkTransitionFilterDto courseLinkTransitionFilterDto);
 
 }
--- src/main/java/com/rmr/dinosaurs/domain/statistics/service/impl/CourseStatisticsServiceImpl.java
+++ src/main/java/com/rmr/dinosaurs/domain/statistics/service/impl/CourseStatisticsServiceImpl.java
@@ -3,20 +3,24 @@ package com.rmr.dinosaurs.domain.statistics.service.impl;
 import static com.rmr.dinosaurs.domain.auth.exception.errorcode.UserErrorCode.USER_NOT_FOUND;
 import static com.rmr.dinosaurs.domain.core.exception.errorcode.CourseErrorCode.COURSE_NOT_FOUND;
 
-import com.rmr.dinosaurs.domain.auth.model.User;
 import com.rmr.dinosaurs.domain.auth.security.model.DinoPrincipal;
 import com.rmr.dinosaurs.domain.core.exception.ServiceException;
-import com.rmr.dinosaurs.domain.core.model.Course;
+import com.rmr.dinosaurs.domain.statistics.configuration.properties.CourseStatisticsProperties;
 import com.rmr.dinosaurs.domain.statistics.model.CourseLinkTransition;
+import com.rmr.dinosaurs.domain.statistics.model.dto.CourseLinkTransitionFilterDto;
+import com.rmr.dinosaurs.domain.statistics.model.dto.CourseLinkTransitionPageDto;
 import com.rmr.dinosaurs.domain.statistics.service.CourseStatisticsService;
+import com.rmr.dinosaurs.domain.statistics.utils.converter.CourseLinkTransitionConverter;
 import com.rmr.dinosaurs.infrastucture.database.auth.UserRepository;
 import com.rmr.dinosaurs.infrastucture.database.core.CourseRepository;
 import com.rmr.dinosaurs.infrastucture.database.statistics.CourseLinkTransitionRepository;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.Objects;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -25,22 +29,50 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CourseStatisticsServiceImpl implements CourseStatisticsService {
 
+  private final CourseStatisticsProperties properties;
+  private final CourseLinkTransitionConverter courseLinkTransitionConverter;
+
   private final CourseLinkTransitionRepository courseLinkTransitionRepository;
   private final CourseRepository courseRepository;
   private final UserRepository userRepository;
 
+
   @Override
   public void courseTransitionRegister(Long courseId) {
+    log.info("Link transition to course with id: {}", courseId);
     getCurrentUserPrincipal().ifPresent(userPrincipal -> {
-          Course course = courseRepository.findById(courseId).orElseThrow(
+          var course = courseRepository.findById(courseId).orElseThrow(
               () -> new ServiceException(COURSE_NOT_FOUND));
-          User user = userRepository.findById(userPrincipal.getId()).orElseThrow(
+          var user = userRepository.findById(userPrincipal.getId()).orElseThrow(
               () -> new ServiceException(USER_NOT_FOUND));
           courseLinkTransitionRepository.save(
-              new CourseLinkTransition(null, course, user, LocalDateTime.now(ZoneOffset.UTC))
+              new CourseLinkTransition(null, course, user,
+                  LocalDateTime.now(ZoneOffset.UTC))
           );
         }
     );
+  }
+
+  @Override
+  public CourseLinkTransitionPageDto getAllCourseLinkTransitionsByFilter(
+      CourseLinkTransitionFilterDto filter) {
+    var page = PageRequest.of(
+        filter.getPage(),
+        Objects.nonNull(filter.getPageSize())
+            ? filter.getPageSize()
+            : properties.getDefaultPageSize()
+    );
+    var courseLinkTransitionPage = courseLinkTransitionRepository.findAllByFilter(
+        filter.getCoursesIds(), filter.getUserEmail(),
+        filter.getTransitionedFrom(), filter.getTransitionedTo(), page);
+    var courseLinkTransitionDtos = courseLinkTransitionPage.stream()
+        .map(courseLinkTransitionConverter::toCourseLinkTransitionDto)
+        .toList();
+    return new CourseLinkTransitionPageDto(courseLinkTransitionPage.getTotalElements(),
+        courseLinkTransitionPage.getTotalPages(),
+        courseLinkTransitionPage.getSize(),
+        courseLinkTransitionPage.getNumber(),
+        courseLinkTransitionDtos);
   }
 
   private Optional<DinoPrincipal> getCurrentUserPrincipal() {
