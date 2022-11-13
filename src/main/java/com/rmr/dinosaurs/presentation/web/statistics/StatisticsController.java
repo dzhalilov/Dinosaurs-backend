d251f16da8ff8c2a4489051204db3d0c56080e1f
ffibonacci <fadedfacet@gmail.com>
2022-11-13 21:36:39 +0300

    add endpoint to get filtered statistics

--- src/main/java/com/rmr/dinosaurs/presentation/web/statistics/StatisticsController.java
+++ src/main/java/com/rmr/dinosaurs/presentation/web/statistics/StatisticsController.java
@@ -1,10 +1,15 @@
 package com.rmr.dinosaurs.presentation.web.statistics;
 
+import com.rmr.dinosaurs.domain.auth.security.permission.ModeratorPermission;
+import com.rmr.dinosaurs.domain.statistics.model.dto.CourseLinkTransitionFilterDto;
+import com.rmr.dinosaurs.domain.statistics.model.dto.CourseLinkTransitionPageDto;
 import com.rmr.dinosaurs.domain.statistics.service.CourseStatisticsService;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +25,15 @@ public class StatisticsController {
   @ResponseStatus(HttpStatus.CREATED)
   public void registerCourseTransition(@PathVariable Long courseId) {
     courseStatisticsService.courseTransitionRegister(courseId);
+  }
+
+  @PostMapping("/course/search")
+  @ModeratorPermission
+  public CourseLinkTransitionPageDto getAllCourseLinkTransitionsByPagesAndFilters(
+      @RequestBody @Valid
+      CourseLinkTransitionFilterDto courseLinkTransitionFilterDto) {
+    return courseStatisticsService.getAllCourseLinkTransitionsByFilter(
+        courseLinkTransitionFilterDto);
   }
 
 }
