aabf91f817b66260ae291b7364a5dc4f820d5940
ffibonacci <fadedfacet@gmail.com>
2022-11-13 21:34:26 +0300

    add course link transition filter dto

---
+++ src/main/java/com/rmr/dinosaurs/domain/statistics/model/dto/CourseLinkTransitionFilterDto.java
@@ -0,0 +1,42 @@
+package com.rmr.dinosaurs.domain.statistics.model.dto;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import javax.annotation.Nonnegative;
+import javax.annotation.Nullable;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourseLinkTransitionFilterDto {
+
+  @NotNull
+  @Nonnegative
+  Integer page;
+
+  @Nullable
+  Integer pageSize;
+
+  @Nullable
+  Set<Long> coursesIds;
+
+  @Nullable
+  String userEmail;
+
+  @NotNull
+  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+  LocalDateTime transitionedFrom;
+
+  @NotNull
+  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+  LocalDateTime transitionedTo;
+
+}
