8414dfa4e187d8a078c3ad7bd29c65dcfaf1ab2e
Ilyas Dzhalilov <dzhalilov.ilyas@gmail.com>
2022-11-15 15:33:57 +0300

    fix email validation

--- src/main/java/com/rmr/dinosaurs/domain/auth/utils/validator/validators/EmailValidator.java
+++ src/main/java/com/rmr/dinosaurs/domain/auth/utils/validator/validators/EmailValidator.java
@@ -8,13 +8,9 @@ import javax.validation.ConstraintValidatorContext;
 public class EmailValidator implements
     ConstraintValidator<EmailConstraintValidator, String> {
 
-  /**
-   * RFC 5322 standard following regex pattern regex from
-   * https://www.baeldung.com/java-email-validation-regex#strict-regular-expression-validation
-   */
   protected static final Pattern emailPattern =
-      Pattern.compile("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
-          + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$");
+      Pattern.compile("^(?![.])([A-Za-z0-9_\\-.](?!.*\\.\\.)){1,64}@"
+          + "([^\\-. ][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*.*(?<!-))(\\.[A-Za-z]{2,})$");
 
   @Override
   public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
